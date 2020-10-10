335e4471f348b56b7cf1384b99dbaabe29aa4f5e
Colocust <516100080@qq.com>
2020-10-10 23:07:43 +0800

    重构main

--- main/main.go
+++ main/main.go
@@ -1,12 +1,49 @@
 package main
 
 import (
-	"tinyQ"
+	"net/http"
 	"tinyQ/config"
+	"tinyQ/route"
 )
 
+func init() {
+	config.Load()
+}
+
+type (
+	App struct {
+		Ip     string
+		Port   string
+		Router *route.Router
+		Server *http.Server
+	}
+)
+
+func NewApp() *App {
+	return &App{
+		Ip:     "127.0.0.1",
+		Port:   "8899",
+		Router: route.NewRouter(),
+	}
+}
+
+func (app *App) Run() error {
+	if app.Server == nil {
+		app.Server = &http.Server{
+			Addr:    app.Ip + ":" + app.Port,
+			Handler: app.Router,
+		}
+	}
+	err := app.Server.ListenAndServe()
+	return err
+}
 
 func main() {
-	config.Load()
-	tinyQ.Boot()
+	app := NewApp()
+	route.Register(app.Router)
+
+	err := app.Run()
+	if err != nil {
+		panic(err)
+	}
 }
