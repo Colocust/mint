8fb95e617849de9568b86aed5875692a688bea17
Colocust <516100080@qq.com>
2020-10-12 17:30:10 +0800

    修改路由器

--- route/router.go
+++ route/router.go
@@ -1,42 +1,51 @@
 package route
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
+	"tinyQ/http/server"
 )
 
-type Router struct {
-	route map[string]map[string]http.HandlerFunc
-}
+type (
+	HandlerFunc func(req *http.Request) server.Response
+
+	Router struct {
+		route map[string]map[string]HandlerFunc
+	}
+)
 
 func NewRouter() *Router {
 	return new(Router)
 }
 
-func (r *Router) handleFunc(method string, path string, handlerFunc http.HandlerFunc) {
+func (r *Router) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	f, ok := r.route[req.Method][req.URL.String()]
+	if ok {
+		response := f(req)
+
+		data, _ := json.Marshal(response)
+		writer.Write(data)
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (r *Router) handleFunc(method string, path string, handlerFunc HandlerFunc) {
 	method = strings.ToUpper(method)
 	if r.route == nil {
-		r.route = make(map[string]map[string]http.HandlerFunc)
+		r.route = make(map[string]map[string]HandlerFunc)
 	}
 	if r.route[method] == nil {
-		r.route[method] = make(map[string]http.HandlerFunc)
+		r.route[method] = make(map[string]HandlerFunc)
 	}
 	r.route[method][path] = handlerFunc
 }
 
-func (r *Router) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	handlerFunc, ok := r.route[request.Method][request.URL.String()]
-	if ok {
-		handlerFunc(writer, request)
-	} else {
-		writer.WriteHeader(404)
-	}
-}
-
-func (r *Router) Get(path string, handlerFunc http.HandlerFunc) {
+func (r *Router) Get(path string, handlerFunc HandlerFunc) {
 	r.handleFunc(http.MethodGet, path, handlerFunc)
 }
 
-func (r *Router) Post(path string, handlerFunc http.HandlerFunc) {
+func (r *Router) Post(path string, handlerFunc HandlerFunc) {
 	r.handleFunc(http.MethodPost, path, handlerFunc)
 }
