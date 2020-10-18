4b420904a6a61eb75e652f83e4c97cd154c34af7
Colocust <516100080@qq.com>
2020-10-18 21:44:37 +0800

    完成task开发

--- task/api.go
+++ task/api.go
@@ -1,5 +1,26 @@
-package main
+package task
 
-func main() {
-	$END$
+import (
+	"mint/http/server"
+	"net/http"
+)
+
+func Add(req *AddTaskRequest) *AddTaskResponse {
+	t := &Task{req.Url, req.Data}
+	go product(t)
+
+	resp := new(AddTaskResponse)
+	resp.Code = http.StatusOK
+	resp.Message = "OK"
+	return resp
 }
+
+type (
+	AddTaskRequest struct {
+		Url  string                 `json:"url"`
+		Data map[string]interface{} `json:"data"`
+	}
+	AddTaskResponse struct {
+		server.Response
+	}
+)
--- task/boot.go
+++ task/boot.go
@@ -1,6 +1,37 @@
 package task
 
+import (
+	"encoding/json"
+	"mint/config"
+	"mint/http/client"
+)
+
+var ch chan *Task
 
 func Boot() {
-
+	taskNum := config.Read("task_num").(float64)
+	ch = make(chan *Task, int(taskNum))
+	consumer()
 }
+
+func consumer() {
+	for {
+		t := <-ch
+		content, _ := json.Marshal(t.data)
+
+		builder := new(client.NetBuilder)
+		sender := builder.SetUrl(t.url).SetContent(string(content)).NewNetSender()
+		sender.Send("POST")
+	}
+}
+
+func product(t *Task) {
+	ch <- t
+}
+
+type (
+	Task struct {
+		url  string
+		data map[string]interface{}
+	}
+)
