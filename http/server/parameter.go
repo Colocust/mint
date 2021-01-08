8104d654de847b057de864dfc67258988c0802f2
Colocust <516100080@qq.com>
2021-01-08 17:25:48 +0800

    定义parameter

--- http/server/parameter.go
+++ http/server/parameter.go
@@ -5,12 +5,11 @@ type (
 		Url     string `json:"url"`
 		Content string `json:"content"`
 	}
-	
+
 	Response struct {
 		Code    int    `json:"code"`
-		Message string `json:"message"`
 	}
-	
+
 	DelayRequest struct {
 		Request
 	}
