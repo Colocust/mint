9729935d3a6ac6ed250a07592a96e3f398d50b31
Colocust <516100080@qq.com>
2021-01-08 15:42:36 +0800

    新增util

---
+++ util/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+)
+
+var (
+	isLoad = false
+	config map[string]interface{}
+)
+
+func Load(name string) error {
+	if isLoad == false {
+		file, err := os.Open(name)
+		if err != nil {
+			return errors.New("can not find " + name)
+		}
+
+		var configString string
+
+		buf := bufio.NewReader(file)
+		for {
+			item, err := buf.ReadString('\n')
+			configString += item
+			if err != nil {
+				if err == io.EOF {
+					break
+				} else {
+					return err
+				}
+			}
+		}
+
+		if err := json.Unmarshal([]byte(configString), &config); err != nil {
+			return err
+		}
+
+		isLoad = true
+	}
+	return nil
+}
+
+func Get(key string) interface{} {
+	return config[key]
+}
